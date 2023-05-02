13c9079ae02197a12c3b0d54d0b467e3f7f2ad0a
PattabiRamanJanaagraha <pattabi.raman@janaagraha.org>
2023-05-02 15:15:46 +0530

    revert to original

--- app/src/main/java/dev/pattabiraman/apputils/webserviceutils/callback/OnResponseListener.java
+++
@@ -1,8 +0,0 @@
-package dev.pattabiraman.apputils.webserviceutils.callback;
-
-import org.json.JSONObject;
-
-public interface OnResponseListener {
-    void OnResponseFailure(JSONObject response);
-    void OnResponseSuccess(JSONObject response);
-}
