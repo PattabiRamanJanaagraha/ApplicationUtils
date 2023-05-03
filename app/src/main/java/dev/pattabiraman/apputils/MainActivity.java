e25f49284cc59b125e3bba79c0126c8deab76e87
PattabiRamanJanaagraha <pattabi.raman@janaagraha.org>
2023-05-03 11:47:32 +0530

    commit before checkout new branch

--- app/src/main/java/dev/pattabiraman/apputils/MainActivity.java
+++ app/src/main/java/dev/pattabiraman/apputils/MainActivity.java
@@ -20,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
         PluginWebserviceHelper.getInstance().runWebService(activity, PluginWebserviceHelper.METHOD_GET, url, requestParams, new OnResponseListener() {
             @Override
