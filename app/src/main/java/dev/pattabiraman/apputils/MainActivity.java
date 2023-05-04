9689a6de916cf8fa43b21cc9f8df2e26f84078f7
PattabiRamanJanaagraha <pattabi.raman@janaagraha.org>
2023-05-04 11:17:08 +0530

    1.0.3 test release

--- app/src/main/java/dev/pattabiraman/apputils/MainActivity.java
+++ app/src/main/java/dev/pattabiraman/apputils/MainActivity.java
@@ -1,13 +1,14 @@
 package dev.pattabiraman.apputils;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import org.json.JSONObject;
 
 import java.util.HashMap;
 
+import dev.pattabiraman.webserviceutils.PluginAppUtils;
 import dev.pattabiraman.webserviceutils.callback.OnResponseListener;
 import dev.pattabiraman.webserviceutils.webservice.PluginWebserviceHelper;
 
@@ -21,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        activity = MainActivity.this;
         PluginWebserviceHelper.getInstance().runWebService(activity, PluginWebserviceHelper.METHOD_GET, url, requestParams, new OnResponseListener() {
             @Override
             public void OnResponseFailure(JSONObject response) {
@@ -30,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void OnResponseSuccess(JSONObject response) {
-
+                PluginAppUtils.getInstance(activity).showToast(activity, response.toString());
             }
         }, false, PluginWebserviceHelper.HEADER_TYPE_NORMAL);
     }
